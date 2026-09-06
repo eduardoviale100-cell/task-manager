bf118856973f8fb453eb4fe3a8f2c443ca0201fc
eduardoviale100-cell <eduardoviale100@gmail.com>
2026-09-05 22:09:32 -0500

    fix: use webEnvironment=NONE and @ActiveProfiles(h2) to fix CI test

--- src/test/java/com/example/task_manager/TaskManagerApplicationTests.java
+++ src/test/java/com/example/task_manager/TaskManagerApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.task_manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("h2")
 class TaskManagerApplicationTests {
 
 	@Test
